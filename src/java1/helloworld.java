905d1d9e37e75aa49cca1da35249197878433d97
zhumengyang <3564715618@qq.com>
2022-03-24 00:49:59 +0800

    first

---
+++ src/java1/helloworld.java
@@ -0,0 +1,36 @@
+package java1;
+
+import java.util.ArrayList;
+
+public class helloworld {
+    public static void main(String[] args){
+        System.out.println("helloworld");
+        ArrayList list =new ArrayList();
+    }
+    public void method(){
+        System.out.println();
+        System.out.println("helloworld.method");
+        System.out.println();
+        System.out.println("true = " + true);
+        System.out.printf("");
+        String[] arr=new String[]{"1","2","3"};
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.println(arr[i]);
+
+        }
+        for (String s : arr) {
+
+        }
+        for (int i = 0; i < arr.length; i++) {
+            String s = arr[i];
+
+        }
+        if (arr == null) {
+
+        }
+        if (arr != null) {
+            if (arr != null) {
+                }
+        }
+}
+}
